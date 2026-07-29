4590e80c3ea1985c17ae0cc9a623868076f40857
elaaasri <anas_elasri@outlook.fr>
2026-07-29 14:14:42 +0100

    feat: add server routes and file paths

---
+++ index.js
@@ -0,0 +1,33 @@
+import http from "node:http";
+import fs from "node:fs";
+
+const server = http.createServer((req, res) => {
+  // sets header content type:
+  res.setHeader("Content-Type", "text/html");
+
+  // sets path:
+  let path = "";
+  switch (req.url) {
+    case "/":
+      path = "./index.html";
+      break;
+    case "/about":
+      path = "./about.html";
+      break;
+    case "/contact-me":
+      path = "./contact-me.html";
+      break;
+    default:
+      break;
+  }
+
+  fs.readFile(path, "utf-8", (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      return res.end("Server Error");
+    }
+    res.end(data);
+  });
+});
+
+server.listen(8080);
